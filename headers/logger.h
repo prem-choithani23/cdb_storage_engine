1c8da2ce8d3ffcc48b5ff62ed639a6d01169558d
prem-choithani <premchoithani4@gmail.com>
2026-07-02 20:11:41 +0530

    Add logging interface declaration in headers/logger.h

--- headers/logger.h
+++ headers/logger.h
@@ -5,7 +5,6 @@
 #ifndef STORAGE_ENGINE_LOGGER_H
 #define STORAGE_ENGINE_LOGGER_H
 
-
-void log(char * from , char * message);
+void logger(const char * from , const char * message);
 
 #endif //STORAGE_ENGINE_LOGGER_H
