0e840baf7f7bbb4d0ab19b2fa35f7733cdb80b4b
prem-choithani <premchoithani4@gmail.com>
2026-07-02 20:14:47 +0530

    Define heap file interface in headers/heap_file.h

--- headers/heap_file.h
+++ headers/heap_file.h
@@ -4,9 +4,25 @@
 
 #ifndef STORAGE_ENGINE_HEAP_FILE_H
 #define STORAGE_ENGINE_HEAP_FILE_H
+#include "disk_manager.h"
+#include "tid.h"
+#include "../headers/slotted_page.h"
+
+typedef struct hf {
+    DiskManager * dm;
+    PageId first_free_page;
+}HeapFile;
+
+
+HeapFile* heap_file_open(const char* path);
+void heap_file_close(HeapFile* hf);
+
+
+TID heap_file_insert(HeapFile* hf, void* data, uint16_t length);
+int heap_file_read(HeapFile* hf, TID tid, void* out, uint16_t length);
+TID heap_file_update(HeapFile* hf, TID tid, void* data, uint16_t length);
+void heap_file_delete(HeapFile* hf, TID tid);
+
 
 #endif //STORAGE_ENGINE_HEAP_FILE_H
 
-typedef struct hf {
-
-}HeapFile;
