fe11423f68ad014cac4275c8fa0d7b544cc1a1e6
prem-choithani <premchoithani4@gmail.com>
2026-07-02 20:14:00 +0530

    Add slotted page API declarations in headers/slotted_page.h

--- headers/slotted_page.h
+++ headers/slotted_page.h
@@ -4,7 +4,14 @@
 
 #ifndef STORAGE_ENGINE_SLOTTED_PAGE_H
 #define STORAGE_ENGINE_SLOTTED_PAGE_H
+#include <stdint.h>
+#include "page.h"
+#include "constants.h"
 
 
+SlotId page_insert(Page * page , void * data ,  uint16_t length);
+int page_read(Page* page, SlotId slot_id, void* out, uint16_t length);
+SlotId page_update(Page* page, SlotId slot_id, void* data, uint16_t length);
+void page_delete(Page * page , SlotId slot_id);
 
 #endif //STORAGE_ENGINE_SLOTTED_PAGE_H
