6b97265aba04c6745ba262a7b9df6d166919b9f6
prem-choithani <premchoithani4@gmail.com>
2026-07-02 20:15:01 +0530

    Add table API interface declarations in headers/table_api.h

---
+++ headers/table_api.h
@@ -0,0 +1,21 @@
+//
+// Created by prem-choithani on 7/1/26.
+//
+
+#ifndef STORAGE_ENGINE_TABLE_API_H
+#define STORAGE_ENGINE_TABLE_API_H
+#include "heap_file.h"
+
+
+typedef struct {
+    HeapFile * hf;
+} TableAPI;
+
+TableAPI  * table_api_open(const char* path);
+void table_api_close(TableAPI * table);
+
+TID insert_data(TableAPI * api , void * data , uint16_t length);
+int read_data(TableAPI * api , TID tid , void *out , uint16_t length);
+TID update_data(TableAPI * api , TID tid , void * data , uint16_t length);
+void delete_data(TableAPI * api, TID tid);
+#endif //STORAGE_ENGINE_TABLE_API_H
